2bcd1acf75fc2ee465d40f52e2e08e57d52a2cad
Muhammad-Asad-Mughal <asadmughal1785@gmail.com>
2023-01-14 20:00:46 +0500

    creating constructors for api

--- app/src/main/java/com/example/newsium/RetrofitAPI.java
+++ app/src/main/java/com/example/newsium/RetrofitAPI.java
@@ -1,4 +1,14 @@
 package com.example.newsium;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
 public interface RetrofitAPI {
+    @GET
+    Call<NewsModal> getAllNews(@Url String url);
+
+    @GET
+    Call<NewsModal> getNewsByCategory(@Url String url);
+
 }
