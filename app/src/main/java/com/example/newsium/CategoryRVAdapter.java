fdf7c67e945588022381aaf5e34bac3ba71f3403
uzumakinaruto7860 <malixbilix786@gmail.com>
2023-01-15 00:16:02 -0800

    completed CategoryRVAdapter starting main activity

--- app/src/main/java/com/example/newsium/CategoryRVAdapter.java
+++ app/src/main/java/com/example/newsium/CategoryRVAdapter.java
@@ -1,22 +1,29 @@
 package com.example.newsium;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.ViewHolder> {
     private ArrayList<CategoryRVModal> categoryRVModals;
     private Context context;
+    private CategoryClickInterface categoryClickInterface;
 
-    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModals, Context context) {
+    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModals, Context context, CategoryClickInterface categoryClickInterface) {
         this.categoryRVModals = categoryRVModals;
         this.context = context;
+        this.categoryClickInterface = categoryClickInterface;
     }
 
     @NonNull
@@ -29,17 +36,34 @@ public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull CategoryRVAdapter.ViewHolder holder, int position) {
-
+        CategoryRVModal categoryRVModal = categoryRVModals.get(position);
+        holder.categoryTV.setText(categoryRVModal.getCategory());
+        Picasso.get().load(categoryRVModal.getCategoryImageUrl()).into(holder.categoryIV);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                categoryClickInterface.onCategoryClick(position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categoryRVModals.size();
+    }
+
+    public interface CategoryClickInterface{
+        void onCategoryClick(int position);
+
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView categoryTV;
+        private ImageView categoryIV;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            categoryTV = itemView.findViewById(R.id.idTVCategory);
+            categoryIV = itemView.findViewById(R.id.idIVCategory);
         }
     }
 }
