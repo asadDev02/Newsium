b1b490a17b838cf511b77f14156047c1fb201a35
uzumakinaruto7860 <malixbilix786@gmail.com>
2023-01-14 22:42:43 -0800

    done NewsRVAdapter

--- app/src/main/java/com/example/newsium/NewsRVAdapter.java
+++ app/src/main/java/com/example/newsium/NewsRVAdapter.java
@@ -1,11 +1,16 @@
 package com.example.newsium;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -22,22 +27,34 @@ public class NewsRVAdapter extends RecyclerView.Adapter<NewsRVAdapter.ViewHolder
     @NonNull
     @Override
     public NewsRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_rv_item, parent, false);
+        return new NewsRVAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NewsRVAdapter.ViewHolder holder, int position) {
+        Articles articles = articlesArrayList.get(position);
+        holder.subTitleTV.setText(articles.getDescription());
+        holder.titleTV.setText(articles.getTitle());
+        Picasso.get().load(articles.getUrlToImage()).into(holder.newsIV);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articlesArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView titleTV, subTitleTV;
+        private ImageView newsIV;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            titleTV = itemView.findViewById(R.id.idTVNewsHeading);
+            subTitleTV = itemView.findViewById(R.id.idTVSubTitle);
+            newsIV = itemView.findViewById(R.id.idIVNews);
+
         }
     }
 }
